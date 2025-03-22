723c2c63f2a62bffb54333c8a0ee6b701037f685
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-03-22 14:35:14 -0300

    test(web): rename tests

--- web/src/test/java/org/gbl/APITest.java
+++ web/src/test/java/org/gbl/APITest.java
@@ -2,6 +2,7 @@ package org.gbl;
 
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,32 +13,36 @@ public class APITest {
 
     private final String validBody = "{\"name\":\"Mario Kart\", \"birthdate\":\"2018-11-15\"}";
 
-    @Test
-    void should_create_a_contact() {
-        JavalinTest.test(app, (server, httpClient) -> {
-            final var response = httpClient.post("/contacts", validBody);
-            assertThat(response.header("Content-Type")).isEqualTo("application/json");
-            assertThat(response.code()).isEqualTo(201);
-        });
-    }
+    @Nested
+    class PostContactsShould {
 
-    @Test
-    void should_return_400_when_payload_is_invalid() {
-        JavalinTest.test(app, (server, httpClient) -> {
-            final var body = "{\"name\":\"\"}";
-            final var response = httpClient.post("/contacts", body);
-            assertThat(response.header("Content-Type")).isEqualTo("application/json");
-            assertThat(response.code()).isEqualTo(400);
-        });
-    }
+        @Test
+        void return_201_when_success() {
+            JavalinTest.test(app, (server, httpClient) -> {
+                final var response = httpClient.post("/contacts", validBody);
+                assertThat(response.header("Content-Type")).isEqualTo("application/json");
+                assertThat(response.code()).isEqualTo(201);
+            });
+        }
 
-    @Test
-    void should_return_422_when_business_error() {
-        JavalinTest.test(app, (server, httpClient) -> {
-            httpClient.post("/contacts", validBody);
-            final var response = httpClient.post("/contacts", validBody);
-            assertThat(response.header("Content-Type")).isEqualTo("application/json");
-            assertThat(response.code()).isEqualTo(422);
-        });
+        @Test
+        void return_400_when_payload_is_invalid() {
+            JavalinTest.test(app, (server, httpClient) -> {
+                final var body = "{\"name\":\"\"}";
+                final var response = httpClient.post("/contacts", body);
+                assertThat(response.header("Content-Type")).isEqualTo("application/json");
+                assertThat(response.code()).isEqualTo(400);
+            });
+        }
+
+        @Test
+        void return_422_when_business_error() {
+            JavalinTest.test(app, (server, httpClient) -> {
+                httpClient.post("/contacts", validBody);
+                final var response = httpClient.post("/contacts", validBody);
+                assertThat(response.header("Content-Type")).isEqualTo("application/json");
+                assertThat(response.code()).isEqualTo(422);
+            });
+        }
     }
 }
