23175fd01336ecd0cb6c130475bb61c7fcf1bad2
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-04-20 11:08:49 -0300

    refactor(cli): extract CLITest base class

---
+++ cli/src/test/java/org/gbl/CLITest.java
@@ -0,0 +1,33 @@
+package org.gbl;
+
+import org.gbl.in.BReminder;
+import org.gbl.in.di.GuiceFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import picocli.CommandLine;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class CLITest {
+    protected CommandLine commandLine;
+    protected ByteArrayOutputStream err;
+    protected ByteArrayOutputStream out;
+
+    @BeforeEach
+    void setUp() {
+        out = new ByteArrayOutputStream();
+        err = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(err));
+        System.setOut(new PrintStream(out));
+        commandLine = new CommandLine(new BReminder(), new GuiceFactory());
+        commandLine.setOut(new PrintWriter(out));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setErr(System.err);
+        System.setOut(System.out);
+    }
+}
--- cli/src/test/java/org/gbl/integration/IntegrationTest.java
+++ cli/src/test/java/org/gbl/integration/IntegrationTest.java
@@ -1,42 +1,14 @@
 package org.gbl.integration;
 
-import org.gbl.in.BReminder;
-import org.gbl.in.di.GuiceFactory;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.gbl.CLITest;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import picocli.CommandLine;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.io.PrintWriter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class IntegrationTest {
-
-    private CommandLine commandLine;
-    private ByteArrayOutputStream err;
-    private ByteArrayOutputStream out;
-
-    @BeforeEach
-    void setUp() {
-        out = new ByteArrayOutputStream();
-        err = new ByteArrayOutputStream();
-        System.setErr(new PrintStream(err));
-        System.setOut(new PrintStream(out));
-        commandLine = new CommandLine(new BReminder(), new GuiceFactory());
-        commandLine.setOut(new PrintWriter(out));
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setErr(System.err);
-        System.setOut(System.out);
-    }
+public class IntegrationTest extends CLITest {
 
     @Nested
     class CreateContact {
@@ -68,7 +40,8 @@ public class IntegrationTest {
         void ok() {
             int exitCode = commandLine.execute("get", "1");
             assertThat(exitCode).isEqualTo(0);
-            assertThat(out.toString()).isEqualTo("Contact => id: 1, name: Bella, birthdate: 13/09/1987");
+            assertThat(out.toString()).isEqualTo("Contact => id: 1, name: Bella, birthdate: " +
+                                                         "13/09/1987");
         }
 
         @Test
