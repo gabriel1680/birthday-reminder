bb738cadb4290c4727b72945504e548620fe3662
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-04-21 15:33:48 -0300

    chore(api): add slf4j impl

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation(project(":core"))
     implementation("com.sparkjava:spark-core:2.7.2")
     implementation("org.json:json:20250107")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
 }
 
 tasks.test {
