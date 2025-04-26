46e63b90e100fdcefe20cfca559ad73519fabb00
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-04-26 11:05:44 -0300

    test(api): add IT for update contact

--- api/src/integration/java/org/gbl/ContactAPI_IT.java
+++ api/src/integration/java/org/gbl/ContactAPI_IT.java
@@ -10,6 +10,7 @@ import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.stringContainsInOrder;
 
 public class ContactAPI_IT extends IntegrationTest {
@@ -52,7 +53,24 @@ public class ContactAPI_IT extends IntegrationTest {
                 .body("data.id", equalTo(id))
                 .body("data.name", stringContainsInOrder("Isaac"))
                 .body("data.birthdate", is("1643-01-04"));
-        ;
+    }
+
+    @Test
+    void given_a_contact_should_update_it() {
+        var response = given()
+                .body(withJson("Isaac Newton", "1643-01-04T00:00:00Z"))
+                .when()
+                .post(RESOURCE_URL)
+                .andReturn();
+        var id = new JSONObject(response.body().print()).getJSONObject("data").getString("id");
+        given()
+                .body(withJson("Albert Einstein", "1879-03-14T00:00:00Z"))
+        .when()
+                .put(RESOURCE_URL + "/" + id)
+        .then()
+                .statusCode(204)
+                .contentType(ContentType.JSON)
+                .body(emptyString());
     }
 
     private static String withJson(String name, String birthdate) {
