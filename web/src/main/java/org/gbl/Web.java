034fe6b80026029b0ba83fc06c310d06684e9c03
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2026-06-28 19:38:08 -0300

    feat(web): serve public directory static files

--- web/src/main/java/org/gbl/Web.java
+++ web/src/main/java/org/gbl/Web.java
@@ -4,6 +4,7 @@ import io.javalin.Javalin;
 import io.javalin.config.JavalinConfig;
 import io.javalin.rendering.template.JavalinJte;
 import org.gbl.common.gateway.ContactsGateway;
+import org.gbl.view.ContactSearchPresenter;
 
 public class Web {
 
@@ -11,12 +12,17 @@ public class Web {
     private final ContactsController controller;
 
     public Web(ContactsGateway contactsGateway) {
-        controller = new ContactsController(contactsGateway);
+        controller = new ContactsController(contactsGateway, new ContactSearchPresenter());
         server = Javalin.create(Web::configureServer);
     }
 
     private static void configureServer(JavalinConfig config) {
         config.fileRenderer(new JavalinJte());
+        config.staticFiles.add(staticFileConfig -> {
+            staticFileConfig.hostedPath = "/";
+            staticFileConfig.directory = "/public";
+            staticFileConfig.mimeTypes.add("text/css", ".css");
+        });
     }
 
     public Javalin getServer() {
