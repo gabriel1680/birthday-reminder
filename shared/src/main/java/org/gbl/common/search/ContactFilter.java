f8309554e20b6dabbdd5d54490375aa69dad1022
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2026-06-28 19:38:51 -0300

    feat(web): add ContactFilter.empty

--- shared/src/main/java/org/gbl/common/search/ContactFilter.java
+++ shared/src/main/java/org/gbl/common/search/ContactFilter.java
@@ -1,4 +1,8 @@
 package org.gbl.common.search;
 
 public record ContactFilter(String name, String birthdateFrom, String birthdateTo) {
+
+    public static ContactFilter empty() {
+        return new ContactFilter("", "", "");
+    }
 }
