5f8b358f3faea112483bbebe954add791f3c4de4
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2026-07-09 21:20:19 -0300

    test(web): extract SearchContactsPage test

--- web/src/test/java/org/gbl/WebTest.java
+++ web/src/test/java/org/gbl/WebTest.java
@@ -10,6 +10,7 @@ import org.gbl.common.search.SortingOrder;
 import org.gbl.common.gateway.ContactResponse;
 import org.gbl.common.gateway.ContactsGateway;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -53,74 +54,78 @@ public class WebTest {
         return LocalDate.parse(dateString, pattern);
     }
 
-    @Test
-    void contacts_page() {
-        JavalinTest.test(server, (server, httpClient) -> {
-            when(contactsGateway.search(any())).thenReturn(success(Pagination.empty()));
-            when(contactsGateway.getUpcomingBirthdays(any())).thenReturn(success(emptyList()));
-            final var response = httpClient.get("/");
-            assertThat(response.header("Content-Type")).isEqualTo("text/html");
-            assertThat(response.code()).isEqualTo(200);
-        });
-    }
+    @Nested
+    class SearchContactsPage {
 
-    @Test
-    void contacts_page_content() {
-        JavalinTest.test(server, (server, httpClient) -> {
-            final var contactResponses = List.of(AYRTON_SENNA, OZZY_OSBURN);
-            final var pagination = new Pagination<>(1, 5, 2, 1, contactResponses);
-            when(contactsGateway.search(any())).thenReturn(success(pagination));
-            when(contactsGateway.getUpcomingBirthdays(any())).thenReturn(success(contactResponses));
-            final var response = httpClient.get("/");
-            assert response.body() != null;
-            final var htmlContent = response.body().string();
-            assertThat(htmlContent).contains("Ozzy");
-            assertThat(htmlContent).contains("03/12/1948");
-        });
-    }
+        @Test
+        void should_send_headers() {
+            JavalinTest.test(server, (server, httpClient) -> {
+                when(contactsGateway.search(any())).thenReturn(success(Pagination.empty()));
+                when(contactsGateway.getUpcomingBirthdays(any())).thenReturn(success(emptyList()));
+                final var response = httpClient.get("/");
+                assertThat(response.header("Content-Type")).isEqualTo("text/html");
+                assertThat(response.code()).isEqualTo(200);
+            });
+        }
 
-    @Captor
-    private ArgumentCaptor<SearchRequest<ContactFilter>> requestCaptor;
+        @Test
+        void should_render_with_contacts() {
+            JavalinTest.test(server, (server, httpClient) -> {
+                final var contactResponses = List.of(AYRTON_SENNA, OZZY_OSBURN);
+                final var pagination = new Pagination<>(1, 5, 2, 1, contactResponses);
+                when(contactsGateway.search(any())).thenReturn(success(pagination));
+                when(contactsGateway.getUpcomingBirthdays(any())).thenReturn(success(contactResponses));
+                final var response = httpClient.get("/");
+                assert response.body() != null;
+                final var htmlContent = response.body().string();
+                assertThat(htmlContent).contains("Ozzy");
+                assertThat(htmlContent).contains("03/12/1948");
+            });
+        }
 
-    @Test
-    void contacts_page_pagination() {
-        JavalinTest.test(server, (server, httpClient) -> {
-            final var pagination = new Pagination<>(2, 1, 2, 2, List.of(AYRTON_SENNA));
-            when(contactsGateway.search(any())).thenReturn(success(pagination));
-            httpClient.get("/?page=2&size=1&order=asc");
-            verify(contactsGateway, times(1)).search(requestCaptor.capture());
-            final var value = requestCaptor.getValue();
-            assertThat(value.page()).isEqualTo(2);
-            assertThat(value.size()).isEqualTo(1);
-            assertThat(value.order()).isEqualTo(SortingOrder.ASC);
-        });
-    }
+        @Captor
+        private ArgumentCaptor<SearchRequest<ContactFilter>> requestCaptor;
 
-    @Test
-    void contacts_page_filter() {
-        JavalinTest.test(server, (server, httpClient) -> {
-            final var pagination = new Pagination<>(2, 1, 2, 2, List.of(AYRTON_SENNA));
-            when(contactsGateway.search(any())).thenReturn(success(pagination));
-            httpClient.get("/?name=xyz&birthdateFrom=12/12/1900&birthdateTo=12/12/1999");
-            verify(contactsGateway, times(1)).search(requestCaptor.capture());
-            final var value = requestCaptor.getValue();
-            assertThat(value.filter())
-                    .isNotNull()
-                    .extracting(ContactFilter::name, ContactFilter::birthdateFrom, ContactFilter::birthdateTo)
-                    .containsExactly("xyz", "12/12/1900", "12/12/1999");
-        });
-    }
+        @Test
+        void should_handle_contacts_pagination() {
+            JavalinTest.test(server, (server, httpClient) -> {
+                final var pagination = new Pagination<>(2, 1, 2, 2, List.of(AYRTON_SENNA));
+                when(contactsGateway.search(any())).thenReturn(success(pagination));
+                httpClient.get("/?page=2&size=1&order=asc");
+                verify(contactsGateway, times(1)).search(requestCaptor.capture());
+                final var value = requestCaptor.getValue();
+                assertThat(value.page()).isEqualTo(2);
+                assertThat(value.size()).isEqualTo(1);
+                assertThat(value.order()).isEqualTo(SortingOrder.ASC);
+            });
+        }
 
-    @Test
-    void contacts_page_error() {
-        JavalinTest.test(server, (server, httpClient) -> {
-            final var randomError = new RuntimeException("Random error");
-            when(contactsGateway.search(any())).thenReturn(failure(randomError));
-            final var response = httpClient.get("/");
-            assertThat(response.header("Content-Type")).isEqualTo("text/html");
-            assertThat(response.body().string()).contains("Internal Server Error");
-            assertThat(response.code()).isEqualTo(500);
-        });
+        @Test
+        void should_handle_filtering() {
+            JavalinTest.test(server, (server, httpClient) -> {
+                final var pagination = new Pagination<>(2, 1, 2, 2, List.of(AYRTON_SENNA));
+                when(contactsGateway.search(any())).thenReturn(success(pagination));
+                httpClient.get("/?name=xyz&birthdateFrom=12/12/1900&birthdateTo=12/12/1999");
+                verify(contactsGateway, times(1)).search(requestCaptor.capture());
+                final var value = requestCaptor.getValue();
+                assertThat(value.filter())
+                        .isNotNull()
+                        .extracting(ContactFilter::name, ContactFilter::birthdateFrom, ContactFilter::birthdateTo)
+                        .containsExactly("xyz", "12/12/1900", "12/12/1999");
+            });
+        }
+
+        @Test
+        void should_handle_internal_server_error() {
+            JavalinTest.test(server, (server, httpClient) -> {
+                final var randomError = new RuntimeException("Random error");
+                when(contactsGateway.search(any())).thenReturn(failure(randomError));
+                final var response = httpClient.get("/");
+                assertThat(response.header("Content-Type")).isEqualTo("text/html");
+                assertThat(response.body().string()).contains("Internal Server Error");
+                assertThat(response.code()).isEqualTo(500);
+            });
+        }
     }
 
     @Test
