4d7eec5e8849315b6e0f1dbb8890aa2f630960ba
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2026-07-23 15:33:54 -0300

    refactor(view): extract hard coded routes to Routes object

--- web/src/main/java/org/gbl/config/Routes.java
+++ web/src/main/java/org/gbl/config/Routes.java
@@ -2,15 +2,27 @@ package org.gbl.config;
 
 public final class Routes {
 
-    public static String notifications() {
-        return "/notifications";
-    }
-
     public static String contactDetails(String id) {
         return "%s/%s".formatted(contacts(), id);
     }
 
     public static String contacts() {
         return "/contacts";
+    }
+
+    public static String notifications() {
+        return "/notifications";
+    }
+
+    public static String deleteNotification(String notificationId) {
+        return "%s/%s/delete".formatted(notifications(),notificationId);
+    }
+
+    public static String createNotification() {
+        return "%s/new".formatted(notifications());
+    }
+
+    public static String notificationDetails(String notificationId) {
+        return "%s/%s".formatted(notifications(),notificationId);
     }
 }
